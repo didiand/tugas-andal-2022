09fc7fa779204c2ab354daf41fbca63a7a1ed3de
Didi Andriawan <79737335+didiand@users.noreply.github.com>
2022-10-02 19:49:27 +0800

    Add files via upload

---
+++ soal3.py
@@ -0,0 +1,18 @@
+print ("==Tugas Pertemuan 1==")
+print ("==Soal No.3==")
+print ("Berapakah Nilai X, X adalah float") 
+print ("Dengan Nilai variabel a,b,c sebagai berikut :")
+print ("a = 25;")
+print ("b = 12;")
+print ("c = 5;")
+print ("Hitunglah X dengan rumus sebagai berikut : x = a / (b + c) % 2")
+
+# Soal Nomor 3
+
+nilaiA = input("Masukan Nilai A = ")
+nilaiB = input("Masukan Nilai B = ")
+nilaiC = input("Masukan Nilai C = ")
+
+nilaiA,nilaiB,nilaiC,type(int)
+nilaiX = int(nilaiA) / (int(nilaiB) + int(nilaiC)) % 2
+print ("Nilai X = ", (nilaiX), type(nilaiX))
